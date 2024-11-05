eb6b5234067bb402bbe5be58bd9c121938adde76
theo-budy <theo@budy.bot>
2024-11-04 16:48:35 -0800

    log the data

--- application-services-1/src/kafka-ob1/services/kafka-ob1-processing/crud-operations.service.ts
+++ application-services-1/src/kafka-ob1/services/kafka-ob1-processing/crud-operations.service.ts
@@ -19,9 +19,6 @@ export class CrudOperationsService {
       process.env.ENV === 'PROD'
         ? 'https://os.budy.bot'
         : 'https://app.budy.bot';
-    this.logger.log(
-      `Posting data to table ${tableEntity} for project ${projectName} at ${baseUrl} with instance name ${instanceName}`,
-    );
     const topic = 'budyos-ob1-system';
     const url = `${baseUrl}/services/kafka/ob1-v2/send-request/${topic}/${instanceName}`;
     const requestBody = {
@@ -49,6 +46,7 @@ export class CrudOperationsService {
       this.logger.log(
         `Fetched data from table ${tableEntity} for project ${projectName}`,
       );
+      this.logger.debug(response.data);
       return response.data;
     } catch (error) {
       this.logger.error(`Error fetching data: ${error.message}`);
@@ -66,9 +64,6 @@ export class CrudOperationsService {
       process.env.ENV === 'PROD'
         ? 'https://os.budy.bot'
         : 'https://app.budy.bot';
-    this.logger.log(
-      `Posting data to table ${tableEntity} for project ${projectName} at ${baseUrl} with instance name ${instanceName}`,
-    );
     const topic = 'budyos-ob1-system';
     const url = `${baseUrl}/services/kafka/ob1-v2/send-request/${topic}/${instanceName}`;
     const requestBody = {
