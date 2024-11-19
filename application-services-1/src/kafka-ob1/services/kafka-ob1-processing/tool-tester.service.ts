7af40a97efad3531868e0c28fc06fe5fe0aef6d2
theo-budy <theo@budy.bot>
2024-11-19 15:43:22 -0800

    more debug

--- application-services-1/src/kafka-ob1/services/kafka-ob1-processing/tool-tester.service.ts
+++ application-services-1/src/kafka-ob1/services/kafka-ob1-processing/tool-tester.service.ts
@@ -1,14 +1,18 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus, Logger } from '@nestjs/common';
 import axios, { AxiosResponse } from 'axios';
 
 @Injectable()
 export class ToolTestingService {
+  private readonly logger = new Logger(ToolTestingService.name);
   async runTest(
     serverUrl: string,
-    activityToolId: string,
+    toolId: string,
     requestBody: any,
   ): Promise<any> {
-    const url = `http://${serverUrl}:5004/tool-testing/test/${activityToolId}`;
+    this.logger.debug(
+      `Running test for tool ${toolId} with request body: ${JSON.stringify(requestBody)}`,
+    );
+    const url = `http://${serverUrl}:5004/tool-testing/test/${toolId}`;
 
     try {
       const response: AxiosResponse = await axios.post(url, requestBody, {
@@ -21,6 +25,7 @@ export class ToolTestingService {
       return response.data;
     } catch (error) {
       // Handle errors and throw an HTTPException
+      this.logger.error('Error running tool', error);
       throw new HttpException(
         error.response?.data || 'Error making request',
         error.response?.status || HttpStatus.INTERNAL_SERVER_ERROR,
