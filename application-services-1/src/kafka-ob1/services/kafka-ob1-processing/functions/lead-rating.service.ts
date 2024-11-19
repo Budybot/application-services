71b782bc11d1352db06e017ae134b216f8244a65
theo-budy <theo@budy.bot>
2024-11-19 15:56:37 -0800

    use describe result in query

--- application-services-1/src/kafka-ob1/services/kafka-ob1-processing/functions/lead-rating.service.ts
+++ application-services-1/src/kafka-ob1/services/kafka-ob1-processing/functions/lead-rating.service.ts
@@ -39,15 +39,25 @@ export class LeadRatingService {
       );
       this.logger.debug(`Describe results: ${JSON.stringify(describeTask)}`);
 
+      const eventFields =
+        JSON.parse(describeEvent.result.body)?.result.fieldNames || [];
+      const taskFields =
+        JSON.parse(describeTask.result.body)?.result.fieldNames || [];
+
+      // Build queries dynamically
+      const eventQuery = `SELECT ${eventFields.join(', ')} FROM Event WHERE WhoId = '${recordId}'`;
+      const taskQuery = `SELECT ${taskFields.join(', ')} FROM Task WHERE WhoId = '${recordId}'`;
+
       // Step 3: Run the activity tool twice:
       // First with { event: true, recordId }
       const activityTask = await this.toolTestingService.runTest(
         serverUrl,
         activityToolId,
         {
-          query: `
-            SELECT Id, WhoId, WhatId, Subject, ActivityDate, Status, Priority, IsHighPriority, OwnerId, Description, IsDeleted, AccountId, IsClosed, CreatedDate, CreatedById, LastModifiedDate, LastModifiedById, SystemModstamp, IsArchived, CallDurationInSeconds, CallType, CallDisposition, CallObject, ReminderDateTime, IsReminderSet, RecurrenceActivityId, IsRecurrence, RecurrenceStartDateOnly, RecurrenceEndDateOnly, RecurrenceTimeZoneSidKey, RecurrenceType, RecurrenceInterval, RecurrenceDayOfWeekMask, RecurrenceDayOfMonth, RecurrenceInstance, RecurrenceMonthOfYear, RecurrenceRegeneratedType, TaskSubtype, CompletedDateTime, Meeting_Outcome__c FROM Task WHERE WhoId = '${recordId}'
-            `,
+          query: taskQuery,
+          //   query: `
+          //     SELECT Id, WhoId, WhatId, Subject, ActivityDate, Status, Priority, IsHighPriority, OwnerId, Description, IsDeleted, AccountId, IsClosed, CreatedDate, CreatedById, LastModifiedDate, LastModifiedById, SystemModstamp, IsArchived, CallDurationInSeconds, CallType, CallDisposition, CallObject, ReminderDateTime, IsReminderSet, RecurrenceActivityId, IsRecurrence, RecurrenceStartDateOnly, RecurrenceEndDateOnly, RecurrenceTimeZoneSidKey, RecurrenceType, RecurrenceInterval, RecurrenceDayOfWeekMask, RecurrenceDayOfMonth, RecurrenceInstance, RecurrenceMonthOfYear, RecurrenceRegeneratedType, TaskSubtype, CompletedDateTime, Meeting_Outcome__c FROM Task WHERE WhoId = '${recordId}'
+          //     `,
         },
       );
       this.logger.debug(`Activity results: ${JSON.stringify(activityTask)}`);
@@ -57,9 +67,10 @@ export class LeadRatingService {
         serverUrl,
         activityToolId,
         {
-          query: `
-            SELECT Id, WhoId, WhatId, WhoCount, WhatCount, Subject, Location, IsAllDayEvent, ActivityDateTime, ActivityDate, DurationInMinutes, StartDateTime, EndDateTime, EndDate, Description, AccountId, OwnerId, CurrencyIsoCode, Type, IsPrivate, ShowAs, IsDeleted, IsChild, IsGroupEvent, GroupEventType, CreatedDate, CreatedById, LastModifiedDate, LastModifiedById, SystemModstamp, IsArchived, RecurrenceActivityId, IsRecurrence, RecurrenceStartDateTime, RecurrenceEndDateOnly, RecurrenceTimeZoneSidKey, RecurrenceType, RecurrenceInterval, RecurrenceDayOfWeekMask, RecurrenceDayOfMonth, RecurrenceInstance, RecurrenceMonthOfYear, ReminderDateTime, IsReminderSet, EventSubtype, IsRecurrence2Exclusion, Recurrence2PatternText, Recurrence2PatternVersion, IsRecurrence2, IsRecurrence2Exception, Recurrence2PatternStartDate, Recurrence2PatternTimeZone, Interested_Product__c, Highest_Decision_Maker_Level_in_Meeting__c, UpdateExpected_Date_Task__c, Meeting_Duration__c, Calendly__InviteeUuid__c, Calendly__IsRescheduled__c, Customer_Total_Monthly_Content_Budgets_S__c, Content_Marketing_Objective__c, Retainer_Actionables__c, Content_Strategy_Actionables__c, Actionable_Sales__c, Actionable_Leadership__c, Overall_Sentiment__c, Meeting_Type__c, Unique_Code__c, Unique_Meeting__c, Other_Participants__c, Updated_Activity_Id__c, Account_Type__c, B__c, Quality_Sentiment__c, Delivery_Sentiment__c, Servicing_Sentiment__c, Meeting_Theme__c, Actionable_KAM__c, Team__c, Count__c, Meeting_Outcome__c, Call_Disposition__c, Call_Direction__c, Call_Notes__c, Call_Duration_seconds__c, Opportunity__c, Revenue_Type__c, Days_to_Opportunity__c, Amount__c, Unique__c, Call_Unique_Code__c, Meeting_Taker__c, Meeting_Status__c, Meeting_Sourced_by__c, First_Attempt__c, Call__c, Connected_Call__c, Updated_Duration_Minutes__c, Due_Date_Update_Check__c, MRR_AMount__c, Closure_Probability__c, Source_of_Meeting__c, Duration__c, Manager_Qualification__c, Opportunity_Owner__c, Sequence_Name__c, Extracted_Event_Id__c, Meeting_Funnel__c FROM Event WHERE WhoId = '${recordId}'
-            `,
+          query: eventQuery,
+          //   query: `
+          //     SELECT Id, WhoId, WhatId, WhoCount, WhatCount, Subject, Location, IsAllDayEvent, ActivityDateTime, ActivityDate, DurationInMinutes, StartDateTime, EndDateTime, EndDate, Description, AccountId, OwnerId, CurrencyIsoCode, Type, IsPrivate, ShowAs, IsDeleted, IsChild, IsGroupEvent, GroupEventType, CreatedDate, CreatedById, LastModifiedDate, LastModifiedById, SystemModstamp, IsArchived, RecurrenceActivityId, IsRecurrence, RecurrenceStartDateTime, RecurrenceEndDateOnly, RecurrenceTimeZoneSidKey, RecurrenceType, RecurrenceInterval, RecurrenceDayOfWeekMask, RecurrenceDayOfMonth, RecurrenceInstance, RecurrenceMonthOfYear, ReminderDateTime, IsReminderSet, EventSubtype, IsRecurrence2Exclusion, Recurrence2PatternText, Recurrence2PatternVersion, IsRecurrence2, IsRecurrence2Exception, Recurrence2PatternStartDate, Recurrence2PatternTimeZone, Interested_Product__c, Highest_Decision_Maker_Level_in_Meeting__c, UpdateExpected_Date_Task__c, Meeting_Duration__c, Calendly__InviteeUuid__c, Calendly__IsRescheduled__c, Customer_Total_Monthly_Content_Budgets_S__c, Content_Marketing_Objective__c, Retainer_Actionables__c, Content_Strategy_Actionables__c, Actionable_Sales__c, Actionable_Leadership__c, Overall_Sentiment__c, Meeting_Type__c, Unique_Code__c, Unique_Meeting__c, Other_Participants__c, Updated_Activity_Id__c, Account_Type__c, B__c, Quality_Sentiment__c, Delivery_Sentiment__c, Servicing_Sentiment__c, Meeting_Theme__c, Actionable_KAM__c, Team__c, Count__c, Meeting_Outcome__c, Call_Disposition__c, Call_Direction__c, Call_Notes__c, Call_Duration_seconds__c, Opportunity__c, Revenue_Type__c, Days_to_Opportunity__c, Amount__c, Unique__c, Call_Unique_Code__c, Meeting_Taker__c, Meeting_Status__c, Meeting_Sourced_by__c, First_Attempt__c, Call__c, Connected_Call__c, Updated_Duration_Minutes__c, Due_Date_Update_Check__c, MRR_AMount__c, Closure_Probability__c, Source_of_Meeting__c, Duration__c, Manager_Qualification__c, Opportunity_Owner__c, Sequence_Name__c, Extracted_Event_Id__c, Meeting_Funnel__c FROM Event WHERE WhoId = '${recordId}'
+          //     `,
         },
       );
       this.logger.debug(`Activity results: ${JSON.stringify(activityEvent)}`);
