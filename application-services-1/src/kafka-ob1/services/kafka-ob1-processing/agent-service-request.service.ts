9999d621654d22703e4e76179a940475515eb950
theo-budy <theo@budy.bot>
2024-11-25 14:46:20 -0800

    update clean transcript

--- application-services-1/src/kafka-ob1/services/kafka-ob1-processing/agent-service-request.service.ts
+++ application-services-1/src/kafka-ob1/services/kafka-ob1-processing/agent-service-request.service.ts
@@ -1,86 +1,8 @@
-// import { Injectable, Logger } from '@nestjs/common';
-// import { KafkaOb1Service } from 'src/kafka-ob1/kafka-ob1.service';
-// import { validateAgentServiceRequestBody } from 'src/interfaces/agent-service.interfaces';
-// @Injectable()
-// export class AgentServiceRequest {
-//   private readonly logger = new Logger(AgentServiceRequest.name);
-
-//   constructor(private readonly kafkaOb1Service: KafkaOb1Service) {}
-
-//   async sendAgentRequest(
-//     systemPrompt: string,
-//     userPrompt: string,
-//     config: {
-//       provider: string;
-//       model: string;
-//       temperature: number;
-//       maxTokens: number;
-//       frequencyPenalty: number;
-//       presencePenalty: number;
-//     },
-//     instanceName: string,
-//     messageKey: string,
-//   ): Promise<any> {
-//     const topic = 'budyos-ob1-agentService';
-//     const tokenThreshold = 2000;
-//     if (systemPrompt.length > tokenThreshold) {
-//       this.logger.warn(
-//         `System prompt exceeds token limit of ${tokenThreshold} tokens`,
-//       );
-//       config.model = 'gpt-4o';
-//       config.maxTokens = 12000;
-//     }
-
-//     const messageInput = {
-//       messageContent: {
-//         functionName: 'LLMgenerateResponse-V1',
-//         functionInput: {
-//           systemPrompt,
-//           userPrompt,
-//           config,
-//         },
-//       },
-//       messageType: 'REQUEST',
-//     };
-//     this.logger.log(`Config: ${JSON.stringify(config)}`);
-
-//     try {
-//       validateAgentServiceRequestBody(
-//         messageInput.messageContent.functionInput,
-//       );
-//     } catch (validationError) {
-//       this.logger.error(`Validation failed: ${validationError.message}`);
-//       throw new Error('Invalid request data; please check your input');
-//     }
-//     try {
-//       const response = await this.kafkaOb1Service.sendRequest(
-//         messageKey,
-//         instanceName,
-//         'agent-services',
-//         'sendAgentRequest',
-//         'system',
-//         messageInput,
-//         'system',
-//         messageKey,
-//         topic,
-//       );
-//       // this.logger.log(
-//       //   `Agent request sent for instance ${instanceName} with user prompt: ${userPrompt}`,
-//       // );
-//       this.logger.debug(response);
-//       return response;
-//     } catch (error) {
-//       this.logger.error(`Error sending agent request: ${error.message}`);
-//       throw new Error('Failed to send agent request');
-//     }
-//   }
-
-// }
 import { Injectable, Logger } from '@nestjs/common';
 import { KafkaOb1Service } from 'src/kafka-ob1/kafka-ob1.service';
 import {
   validateExecutePromptRequestBody,
-  ExecutePromptRequestDto,
+  validateAgentServiceRequestBody,
 } from 'src/interfaces/agent-service.interfaces';
 import {
   OB1Global,
@@ -104,13 +26,71 @@ export class AgentServiceRequest {
       frequencyPenalty: number;
       presencePenalty: number;
     },
-    instanceName: string,
-    messageKey: string,
+    userOrgId: string,
+    personId: string,
   ): Promise<any> {
-    this.logger.debug(
-      'Sending agent request function currently not implemented',
-    );
-    return null;
+    const topic = 'budyos-ob1-agentService';
+    const tokenThreshold = 2000;
+    if (systemPrompt.length > tokenThreshold) {
+      this.logger.warn(
+        `System prompt exceeds token limit of ${tokenThreshold} tokens`,
+      );
+      config.model = 'gpt-4o';
+      config.maxTokens = 16384;
+    }
+
+    // Replace empty prompts with a placeholder
+    if (!systemPrompt) {
+      systemPrompt = '[No system prompt provided]';
+    }
+    if (!userPrompt) {
+      userPrompt = '[No user prompt provided]';
+    }
+
+    const messageInput = {
+      messageContent: {
+        functionName: 'LLMgenerateResponse-V1',
+        functionInput: {
+          systemPrompt,
+          userPrompt,
+          config,
+        },
+      },
+      messageType: 'REQUEST',
+    };
+    this.logger.log(`Config: ${JSON.stringify(config)}`);
+
+    const headers: OB1Global.MessageHeaderV2 = {
+      sourceService: process.env.SERVICE_NAME || 'unknown-service',
+      schemaVersion: CURRENT_SCHEMA_VERSION,
+      sourceFunction: 'sendPromptExecutionRequest',
+      sourceType: 'service',
+      destinationService: 'agent-services',
+      requestId: `REQ-sendPromptExecutionRequest-${Date.now()}`,
+      personId,
+      userOrgId,
+    };
+
+    try {
+      validateAgentServiceRequestBody(
+        messageInput.messageContent.functionInput,
+      );
+    } catch (validationError) {
+      this.logger.error(`Validation failed: ${validationError.message}`);
+      throw new Error('Invalid request data; please check your input');
+    }
+    try {
+      const response = await this.kafkaOb1Service.sendRequest(
+        messageInput,
+        headers,
+        topic,
+      );
+      this.logger.debug(response);
+      return response;
+    } catch (error) {
+      this.logger.error(`Error sending agent request: ${error.message}`);
+      throw new Error('Failed to send agent request');
+    }
   }
 
   // async sendPromptExecutionRequest(
