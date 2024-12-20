55ceca3b25b683e8796e4dca00f6978ffd699a6e
jae-budy <jae@budy.bot>
2024-12-20 15:32:17 -0800

    test commit

--- application-services-1/src/app.module.ts
+++ application-services-1/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { KafkaOb1Module } from './kafka-ob1/kafka-ob1.module';
 import { ConfigModule } from '@nestjs/config';
 
+//comment test
 @Module({
   imports: [
     ConfigModule.forRoot({
